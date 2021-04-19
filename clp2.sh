b8b6278798a60ccd016bb1fc2e14aa942d7d1170
Monower Sadaf <monowersadaf@gmail.com>
2021-04-19 11:14:03 +0600

    added answer to the question no 1 of clp 2

---
+++ clp2.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/bash
+
+
+echo "enter how many numbers you want to enter"
+read a
+echo "now plz enter $a numbers"
+
+for ((i=0;i<$a;i++))
+do
+read b[$i]
+done
+
+for ((i=0;i<$a;i++))
+do
+    for ((j=i+1;j<$a;j++))
+    do
+        if ((${b[$i]} < ${b[$j]}))
+        then
+            temp=${b[$i]}
+            ${b[$i]} = ${b[$j]}
+            ${b[$j]} = $temp
+        fi
+    done
+
+
+done
+
+
+sum = (( ${b[1]} + ${b[2]} ))
+echo "The sum of 2nd and 3rd highest number is: $sum"
